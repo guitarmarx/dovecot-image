b369a8f0c14e834fc8a848c46e642b6e827196e3
Marcus Kastner <marcus.kastner@meteorit-leipzig.de>
2021-10-30 00:18:24 +0200

    enabled auth bind

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,7 @@ set -e
 
 ldap_config="/etc/dovecot/dovecot-ldap.conf.ext"
 sed -i "s|#hosts =|hosts = '$LDAP_HOST'|g" $ldap_config
+sed -i "s|#auth_bild =.*|auth_bind = yes|g" $ldap_config
 sed -i "s|base =|base = '$LDAP_BASE'|g" $ldap_config
 
 cp /srv/templates/dovecot.conf /etc/dovecot/dovecot.conf
