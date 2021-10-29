758f1decbf5e4a3f4d639f17df63c28ab3602ec1
Marcus Kastner <marcus.kastner@meteorit-leipzig.de>
2021-10-30 00:21:04 +0200

    changed filter

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,8 +7,12 @@ ldap_config="/etc/dovecot/dovecot-ldap.conf.ext"
 sed -i "s|#hosts =|hosts = '$LDAP_HOST'|g" $ldap_config
 sed -i "s|#auth_bild =.*|auth_bind = yes|g" $ldap_config
 sed -i "s|base =|base = '$LDAP_BASE'|g" $ldap_config
+sed -i "s|#user_filter.*|user_filter = (&(objectClass=posixAccount)(uid=%n))|g" $ldap_config
+sed -i "s|#pass_filter.*|user_filter = (&(objectClass=posixAccount)(uid=%n))|g" $ldap_config
 
 cp /srv/templates/dovecot.conf /etc/dovecot/dovecot.conf
 
 #start dovecot
 dovecot -F  2>&1 |  tee /var/log/dovecot.log
+
+test_dovecot.1.m4wbbxbw1eqhl4ee2n2uxv4xm
